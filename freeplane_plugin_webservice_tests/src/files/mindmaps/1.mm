3ea5bc621deb9614998a89a82efe50893a284a95
Julius S <mail@julius-seltenheim.com>
2013-01-12 13:21:18 +0100

    adjusted testing project

--- freeplane_plugin_webservice_tests/src/files/mindmaps/1.mm
+++ freeplane_plugin_webservice_tests/src/files/mindmaps/1.mm
@@ -1,4 +1,4 @@
-<map version="docear 1.0" dcr_id="1354749378745_eqv7m1gl2vyuxudrlgigsdgsd" dcr_uri="workspace:/../../Temp/Docear/1.mm">
+<map version="docear 1.0" dcr_id="1" dcr_uri="workspace:/../../Temp/Docear/1.mm">
 <!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
 <node TEXT="foo2" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1353101417444" LINK="#ID_510509733"><hook NAME="MapStyle" zoom="0.825">
     <properties show_icon_for_attributes="true" show_note_icons="true"/>
