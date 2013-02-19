0821954a9c16632061736e65051066111a14ea0a
Julius S <mail@julius-seltenheim.com>
2013-02-19 10:55:15 +0100

    correct testing for akka

--- freeplane_plugin_webservice_tests/src/tests/AkkaTests.java
+++ freeplane_plugin_webservice_tests/src/tests/AkkaTests.java
@@ -7,12 +7,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.concurrent.Semaphore;
 
+import org.freeplane.plugin.webservice.Messages.AddNodeRequest;
+import org.freeplane.plugin.webservice.Messages.AddNodeResponse;
 import org.freeplane.plugin.webservice.Messages.CloseMapRequest;
 import org.freeplane.plugin.webservice.Messages.ErrorMessage;
 import org.freeplane.plugin.webservice.Messages.MindmapAsJsonReponse;
 import org.freeplane.plugin.webservice.Messages.MindmapAsJsonRequest;
-import org.freeplane.plugin.webservice.Messages.AddNodeRequest;
-import org.freeplane.plugin.webservice.Messages.AddNodeResponse;
 import org.freeplane.plugin.webservice.Messages.OpenMindMapRequest;
 import org.freeplane.plugin.webservice.actors.MainActor;
 import org.freeplane.plugin.webservice.v10.Webservice;
@@ -22,14 +22,11 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import scala.concurrent.duration.Duration;
-
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.testkit.JavaTestKit;
-import akka.testkit.TestActorRef;
 
 import com.typesafe.config.ConfigFactory;
 
@@ -44,7 +41,7 @@ public class AkkaTests {
 	public static void setUpBeforeClass() throws Exception {
 		system = ActorSystem.create("actoruser", ConfigFactory.load().getConfig("local"));
 		remoteActor = system.actorFor("akka://freeplaneRemote@127.0.0.1:2553/user/main");
-		localActor = system.actorOf(new Props(MainActor.class), "localActor");
+		localActor = system.actorOf(new Props(TheActor.class), "localActor");
 	}
 
 	@AfterClass
@@ -68,7 +65,7 @@ public class AkkaTests {
 			}
 		};
 	}
-	
+
 	@Test
 	public void testAddNodeRequest() {
 		new JavaTestKit(system) {
@@ -76,7 +73,7 @@ public class AkkaTests {
 				new Within(duration("3 seconds")) {
 					protected void run() {
 						sendMindMapToServer(1);
-						remoteActor.tell(new AddNodeRequest("1.mm", "ID_1723255651"), getRef());
+						remoteActor.tell(new AddNodeRequest("1", "ID_1723255651"), localActor);
 
 						AddNodeResponse response = expectMsgClass(AddNodeResponse.class);
 						System.out.println(response.getNode().nodeText);
@@ -86,89 +83,175 @@ public class AkkaTests {
 			}
 		};
 	}
-	
-	@Test
-	@Ignore
-	public void test() {
-		
-		// final Props props = new Props(MainActor.class);
-		// final TestActorRef<MainActor> ref = TestActorRef.create(system,
-		// props, "testA");
-		// final MainActor actor = ref.underlyingActor();
 
+
+	@Test
+	public void sendMapGetAsJsonAndCloseOnServerTest() {
 		new JavaTestKit(system) {
 			{
-				// final Props props = new Props(MainActor.class);
-				final ActorRef remoteActor = system
-						.actorFor("akka://freeplaneRemote@127.0.0.1:2553/user/main");
+				new Within(duration("4 seconds")) {
+					public void run() {
+						sendMindMapToServer(1);
 
-				// final JavaTestKit probe = new JavaTestKit(system);
+						remoteActor.tell(new MindmapAsJsonRequest("1"), getRef());
 
-				remoteActor.tell(new MindmapAsJsonRequest("test_1"), getRef());
+						MindmapAsJsonReponse response = expectMsgClass(MindmapAsJsonReponse.class);
+						System.out.println(response.getJsonString());
+						Assert.assertTrue(response.getJsonString().contains("\"root\":{\"id\":\"ID_1723255651\",\"nodeText\":\"foo2\""));
 
-				expectMsgAllOf(Object.class);
-				// expectMsgEquals(duration("5 seconds"), arg1)
+
+						closeMindMapOnServer(1);
+					}
+				};
 			}
 		};
+
 	}
 
-	 
 	@Test
 	@Ignore
 	public void simulateMultipleUser() {
+		//new JavaTestKit(system) {{
+
 		finishSemaphore = new Semaphore(-3);
+
 		for (int i = 1; i <= 4; i++) {
 			final int mapId = i == 4 ? 5 : i;
 			new Thread(new Runnable() {
 
+
 				@Override
 				public void run() {
+					//						new Within(duration("5 seconds")) {
+					//							public void run() {
 					sendMindMapToServer(mapId);
 
 					localActor.tell(new MindmapAsJsonRequest(mapId + "", 5),
 							localActor);
 
-					closeMindMapOnServer("" + mapId);
+					closeMindMapOnServer(mapId);
 
 					finishSemaphore.release();
+					//							}
+					//						}
+
 				}
 			}).start();
 		}
 
 		finishSemaphore.acquireUninterruptibly();
+		//}};
 	}
 
-	public void sendMindMapToServer(int id) {
-		// InputStream in =
-		// Webservice.class.getResourceAsStream("/files/mindmaps/"+id+".mm");
-		URL pathURL = Webservice.class.getResource("/files/mindmaps/" + id + ".mm");
-		File f = null;
+	@Test
+	public void simulateMultipleUserAkka() {
+		//final ActorRef remoteActor = system.actorFor("akka://freeplaneRemote@127.0.0.1:2553/user/main");
+		new JavaTestKit(system) {{
+			//final JavaTestKit probe = new JavaTestKit(system);
+			localActor.tell(getRef(),getRef());
+			
+			new Within(duration("2 seconds")) {
+				
+				public void run() {
+					sendMindMapToServer(1);
+					sendMindMapToServer(2);
+					sendMindMapToServer(3);
+					sendMindMapToServer(5);
+					
+					
+					MindmapAsJsonReponse response;
+					
+					remoteActor.tell(new MindmapAsJsonRequest(1 + "", 5),localActor);
+					//probe.
+					response = expectMsgClass(MindmapAsJsonReponse.class);
+					System.out.println(response.getJsonString());
+					Assert.assertTrue(response.getJsonString().contains("\"root\":{\"id\":\"ID_1723255651\",\"nodeText\":\"foo2\""));
+					
+					remoteActor.tell(new MindmapAsJsonRequest(2 + "", 5),localActor);
+					response = expectMsgClass(duration("2 seconds"),MindmapAsJsonReponse.class);
+					System.out.println(response.getJsonString());
+					Assert.assertTrue(response.getJsonString().contains("\"id\":\"2.mm\",\"isReadonly\":false,\"root\":{\"id\":\"ID_1723255651\",\"nodeText\":\"New Mindmap\""));
+					
+					remoteActor.tell(new MindmapAsJsonRequest(3 + "", 5),localActor);
+					response = expectMsgClass(duration("2 seconds"),MindmapAsJsonReponse.class);
+					System.out.println(response.getJsonString());
+					Assert.assertTrue(response.getJsonString().contains("\"id\":\"3.mm\",\"isReadonly\":false,\"root\":{\"id\":\"ID_1723255651\",\"nodeText\":\"Welcome\""));
+					
+					remoteActor.tell(new MindmapAsJsonRequest(5 + "", 5),localActor);
+					response = expectMsgClass(duration("2 seconds"),MindmapAsJsonReponse.class);
+					System.out.println(response.getJsonString());
+					Assert.assertTrue(response.getJsonString().contains("\"id\":\"5.mm\",\"isReadonly\":false,\"root\":{\"id\":\"ID_1723255651\",\"nodeText\":\"test_5 = MapID ; 5.mm = Title\""));
+
+
+					
+
+					closeMindMapOnServer(1);
+					closeMindMapOnServer(2);
+					closeMindMapOnServer(3);
+					closeMindMapOnServer(5);
+
+				}
+			};
+
+		}};
+	}
+
+	public void sendMindMapToServer(final int id) {
+		final URL pathURL = Webservice.class.getResource("/files/mindmaps/" + id + ".mm");
+
 		try {
-			f = new File(pathURL.toURI());
+			final File f = new File(pathURL.toURI());
+			final OpenMindMapRequest request = new OpenMindMapRequest(f);
+
+			assertThat(f).isNotNull();
+
+			new JavaTestKit(system) {
+				{
+					new Within(duration("5 seconds")) {
+						public void run() {
+							remoteActor.tell(request, localActor);
+							//expectNoMsg();
+						}
+					};
+				}
+			};
+
 		} catch (URISyntaxException e) {
 		}
 
-		OpenMindMapRequest request = new OpenMindMapRequest(f);
 
-		String contentDeposition = "attachement; filename=\"" + id + ".mm\"";
-		assertThat(f).isNotNull();
-
-		remoteActor.tell(request, localActor);
 	}
 
-	public void closeMindMapOnServer(String id) {
-		remoteActor.tell(new CloseMapRequest(id + ""), localActor);
+	public void closeMindMapOnServer(final int id) {
+		new JavaTestKit(system) {
+			{
+				new Within(duration("2 seconds")) {
+					public void run() {
+						remoteActor.tell(new CloseMapRequest(id+""), localActor);
+						//expectNoMsg();
+					}
+				};
+			}
+		};
+
 
 	}
 
 	public static class TheActor extends UntypedActor {
+		ActorRef target;
 		@Override
 		public void onReceive(Object message) throws Exception {
-			getSender().tell(new Object(), getSelf());
+			System.out.println(message.getClass().getName() + " received");
 
 			if (message instanceof ErrorMessage) {
 				System.out.println(message);
 				org.fest.assertions.Fail.fail("An error occured", ((ErrorMessage) message).getException());
+			}
+			
+			else if (message instanceof ActorRef) {
+				target = (ActorRef)message;
+			} else {
+				target.tell(message, getSelf());
 			}
 
 		}
