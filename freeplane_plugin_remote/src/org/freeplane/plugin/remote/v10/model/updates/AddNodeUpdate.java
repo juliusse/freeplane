47c77419bdbd6325c3aa3d41246032b1a76bec64
Julius Seltenheim <mail@julius-seltenheim.com>
2013-04-08 10:42:47 +0200

    closing Docear/HTW-Frontend#68 ; Added side parameter and added it to documentation

--- freeplane_plugin_remote/src/org/freeplane/plugin/remote/v10/model/updates/AddNodeUpdate.java
+++ freeplane_plugin_remote/src/org/freeplane/plugin/remote/v10/model/updates/AddNodeUpdate.java
@@ -1,22 +1,45 @@
 package org.freeplane.plugin.remote.v10.model.updates;
 
 public class AddNodeUpdate extends MapUpdate {
+	public enum Side {Left, Right}
 
 	private final String parentNodeId;
 	private final String nodeAsJson;
+	private final Side side;
 
 	// Do not remove! Needed for jackson (in testing project)
+	@SuppressWarnings("unused")
 	private AddNodeUpdate() {
-		super(Type.AddNode);
-		parentNodeId = "";
-		nodeAsJson = "";
+		this("","",null);
 	}
-	
+
+
+	/**
+	 * Constructor for default nodes (don't need side)
+	 */
 	public AddNodeUpdate(String parentNodeId, String nodeAsJson) {
+		this(parentNodeId, nodeAsJson, null);
+	}
+
+
+	/**
+	 * For node that is added to root node
+	 * @param side only required at root node level
+	 */
+	public AddNodeUpdate(String parentNodeId, String nodeAsJson, Side side) {
 		super(Type.AddNode);
 		this.parentNodeId = parentNodeId;
 		this.nodeAsJson = nodeAsJson;
+		this.side = side;
 	}
+
+
+
+	public Side getSide() {
+		return side;
+	}
+
+
 
 	public String getNodeAsJson() {
 		return nodeAsJson;
@@ -25,5 +48,5 @@ public class AddNodeUpdate extends MapUpdate {
 	public String getParentNodeId() {
 		return parentNodeId;
 	}
-	
+
 }
